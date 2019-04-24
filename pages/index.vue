14a545103430f07b2c8312823e63a7fbaad7f30e
Justine Carreon <justinecarreon@Peregrine.local>
2019-04-24 17:46:21 +0800

    firstcommit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="home-page">
+    <TheHeader />
+    <hero />
+    <downarrow />
+  </div>
+</template>
+
+<script>
+
+import TheHeader from "~/components/TheHeader.vue"
+import hero from "~/components/hero"
+import downarrow from "~/components/downarrow"
+export default{
+  name: "HomePage",
+  components: {
+    TheHeader,
+    hero,
+    downarrow
+  }
+}
+</script>
+
+<style>
+
+</style>
+
